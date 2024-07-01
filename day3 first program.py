e58e8ad36bd01906606bea1bd8498649fedd9197
SUJIT KUMAR SAHOO <89939924+Sujitsahoo20@users.noreply.github.com>
2024-07-02 00:33:06 +0530

    Add files via upload

---
+++ day3 first program.py
@@ -0,0 +1,28 @@
+""" print("Hello World") # we have to print string b/w (" ")
+
+# print(Hello World) will give us error because python did recognize the string
+
+print(50)
+
+print("Sujit" , 22)
+
+print(17*13) """
+
+#COMMENTS , ESCAPE SEQUENCE(\n) AND PRINT STATEMENT
+
+# Lets start the code ----- this is comment
+# for single line comment we use (#) and for multi line we use (""" """)
+# SHORTCUT KEY FOR COMMENT - CTRL + / (FORWARD SLASH) 
+
+print("Hey I am a good boy \nand this viewer is also a good boy")
+
+#Escape sequence(\n) is used to print the sentence in new line
+
+print("Hey", 6 , 9, sep="-") # sep is used to seperate the print element eg. Hey-6-9
+
+print("Hey", 6 , 9, sep="-", end=" \n") #end is used for the addition of any string at the end of the output of the python print statement.
+print("Sujit")
+
+
+
+
