32ab10ce1a6024315a2c25576ed932b1e5c4918e
SUJIT KUMAR SAHOO <89939924+Sujitsahoo20@users.noreply.github.com>
2024-07-01 19:23:05 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,30 @@
+""" 
+What is Python ?
+
+- Python is dynamically typed general purpose programming language that supports 
+  an object oriented programming approach as well as a functional programming approach.
+
+- Python is an interpreted and a high level programming language.
+
+- It was created by Guido Van Rossum in 1989.
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Features of Python
+
+- Python is simple and easy to understand.
+
+- It is intrepreted and plateform independent which makes debugging very easy.
+
+- Python is open source programming language.
+
+- Python support very big library support. Sone of the popular libraries include Numpy, Seaborn, Tensorflow, OpenCV etc.
+
+- It is possible to integrate other programming language within Python.
+
+"""
+print("Hello world")
+
+print (5)
+
+print(5+5)
