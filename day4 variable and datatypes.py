1e4b1b562354a6393ef79fdcbea592eded41c999
SUJIT KUMAR SAHOO <89939924+Sujitsahoo20@users.noreply.github.com>
2024-07-02 20:17:48 +0530

    Add files via upload

---
+++ day4 variable and datatypes.py
@@ -0,0 +1,36 @@
+"""
+VARIABLES- 
+-Variable is like a container that holds data.
+-Very similar to how our containers in kitchens holds sugar, salt etc.
+-Creating a variable is like creating a placeholder in memory and assigning it
+ some value.
+
+ eg. a = 1
+     b = True etc here a and b are container(variable) and 1 and True is value
+
+"""
+
+a = 1
+print(a)
+b= "Sujit"
+print(b)
+
+"""
+Data Type-
+
+Data type specifies the type of value a variable holds. This is required in programming to do various
+operations without causing an error.
+
+"""
+
+a= 1
+print(type(a))  #int
+
+b = True
+print(type(b)) #bool
+
+c = "Sujit"
+print(type(c)) #str
+
+d = 1.56
+print(type(d)) #float
