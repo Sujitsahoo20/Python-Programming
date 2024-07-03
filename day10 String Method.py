c5560d559b3c96f84e7c96f0001e077bd8f1e686
SUJIT KUMAR SAHOO <89939924+Sujitsahoo20@users.noreply.github.com>
2024-07-04 00:02:12 +0530

    Add files via upload

---
+++ day10 String Method.py
@@ -0,0 +1,22 @@
+#String are immutable
+a = "Sujit.........."
+print(a)
+
+print(len(a)) #print the String len
+
+print(a.upper()) #print the String in Upper case
+
+print(a.lower()) #print the String lower case
+
+print(a.rstrip(".")) #Remove the . from the string
+
+print(a.replace("Sujit","Harshit")) #replace the string
+
+x = "Silver Spoon Sugar Rice"
+print(x.split(" ")) #it separate the strings as a list iteams
+
+y = "hello"
+print(y.capitalize()) #it convert the 1st letter of the string in uppercase
+
+z = "Welcome to our Website"
+print(z.center(50)) #it aligns the string in center of the page
