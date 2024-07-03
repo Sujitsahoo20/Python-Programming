e299b23fa4e0cc93e1f741be7921559ed163ccbd
SUJIT KUMAR SAHOO <89939924+Sujitsahoo20@users.noreply.github.com>
2024-07-03 23:12:52 +0530

    Add files via upload

---
+++ day9 String Slicing.py
@@ -0,0 +1,17 @@
+name = "Sujit, Sahoo"
+
+print(len(name))
+print((name[0:3]))
+
+n = "Harry"
+print(n[-4:-2])
+
+#Explaination
+#len of string - 4 
+# 5-4 = 1
+
+#len of string - 2
+# 5-2 = 3
+# So, [1:3]
+
+print(n[1:3]) # same as we get in n[-4:-2]
