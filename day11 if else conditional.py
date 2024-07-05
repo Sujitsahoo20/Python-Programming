5612e899971082a4872323753da5395de335c130
SUJIT KUMAR SAHOO <89939924+Sujitsahoo20@users.noreply.github.com>
2024-07-05 21:35:56 +0530

    Add files via upload

---
+++ day11 if else conditional.py
@@ -0,0 +1,22 @@
+# a = int(input("Enter your age: "))
+# print("Your age is : ",a)
+
+# if(a>18):
+#     print("car chala skta hai re tu")
+# else:
+#     print("phele bada hoja")
+
+#elif
+
+number = int(input("Enter your number here: "))
+
+if number > 0:
+    print('Positive number')
+
+elif number <0:
+    print('Negative number')
+
+else:
+    print('Zero')
+
+print('This statement is always executed')
